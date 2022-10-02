1351a820da99d551aa96712ce4c2d026109e6e82
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-02 21:20:36 +0300

    Modify file.

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -3,7 +3,7 @@ import BotCard from "./BotCard";
 
 function BotCollection( {bots} ) {
   // Your code here
-  const botCollection = Object.values(bots).map( bot => <BotCard key={bot.id} bot={bot} />)
+  const botCollection = bots.map( bot => <BotCard key={bot.id} bot={bot} />)
   return (
     <div className="ui four column grid">
       <div className="row">
