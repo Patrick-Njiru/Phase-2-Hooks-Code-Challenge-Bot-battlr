87541952f27167897d29679cf3c17b5e4805c48f
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-02 21:23:57 +0300

    Import  and pass props to BotCard child component.

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,13 +1,14 @@
 import React from "react";
+import BotCard from "./BotCard";
 
 function YourBotArmy( {bots} ) {
   //your bot army code here...
-
+  const yourBotArmy = bots.map( bot => <BotCard key={bot.id} bot={bot} />)
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
+          {yourBotArmy}
           Your Bot Army
         </div>
       </div>
