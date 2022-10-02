faee35e65b305d7573e8f206e9f862762014812f
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-03 00:02:50 +0300

    Modify file

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,9 +1,10 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function BotCollection( {bots, selectBot, deleteBot} ) {
+function BotCollection( {bots, selectBot, deleteBot } ) {
   // Your code here
-  const botCollection = bots.map( bot => <BotCard   key={bot.id} handleDelete={deleteBot} handleClick={selectBot} bot={bot} />)
+  const botCollection = bots.map( bot => 
+  <BotCard key={bot.id} handleDelete={deleteBot} handleClick={selectBot} bot={bot} />)
   return (
     <div className="ui four column grid">
       <div className="row">
