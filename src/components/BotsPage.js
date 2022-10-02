1f1bc2c3f935e005d35bf2b246cae4d79b85d474
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-02 18:05:32 +0300

    Fetch data from server and set State using the data. Pass down props to children components.

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
+  const [bots, setBots] = useState([])
   //start here with your code for step one
+  useEffect(() => {
+    fetch('http://localhost:8002/bots')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setBots(data)
+    })
+  })
 
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy bots={bots} />
+      <BotCollection bots={bots} />
     </div>
   )
 }
