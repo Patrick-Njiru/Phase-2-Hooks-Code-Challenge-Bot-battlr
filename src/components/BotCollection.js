3d89524632adc36fef3d2e5b434fdf89af7cc5b2
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-02 22:55:33 +0300

    Add props and pass them down to child component.

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,9 +1,9 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function BotCollection( {bots} ) {
+function BotCollection( {bots, selectBot, deleteBot} ) {
   // Your code here
-  const botCollection = bots.map( bot => <BotCard key={bot.id} bot={bot} />)
+  const botCollection = bots.map( bot => <BotCard   key={bot.id} deleteBot={deleteBot} selectBot={selectBot} bot={bot} />)
   return (
     <div className="ui four column grid">
       <div className="row">
