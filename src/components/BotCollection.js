7c0861c02a329803bdb9cccedc40d04c1bc34a18
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-02 20:07:52 +0300

    Import child component and pass props to display on the browser from parent.

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,13 @@
 import React from "react";
+import BotCard from "./BotCard";
 
 function BotCollection( {bots} ) {
   // Your code here
+  const botCollection = Object.values(bots).map( bot => <BotCard key={bot.id} bot={bot} />)
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
+        {botCollection}
         Collection of all bots
       </div>
     </div>
