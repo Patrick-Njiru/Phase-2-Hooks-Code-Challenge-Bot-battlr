8872e5df41ea5a77ce4f27a25fe0b005d781c8ca
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-03 11:41:46 +0300

    Import and add child component. Create a function to for sorting data and pass it down as a prop to imported child.

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
+import SortBar from "./SortBar";
 
 function BotsPage() {
+  
   const [bots, setBots] = useState([])
-  //start here with your code for step one
   useEffect(() => {
     fetch('http://localhost:8002/bots')
     .then(res => res.json())
@@ -27,9 +28,26 @@ function BotsPage() {
     .catch(console.log)  
   }
   
+  const handleSort = (e) => {
+    
+    if(e.target.name === "health") {
+      fetch('http://localhost:8002/bots')
+      .then(res => res.json())
+      .then(data => setBots(data.sort((bot1, bot2) => bot2.health - bot1.health)))
+    } else if(e.target.name === "damage") {
+      fetch('http://localhost:8002/bots')
+      .then(res => res.json())
+      .then(data => setBots(data.sort((bot1, bot2) => bot2.damage - bot1.damage)))
+    } else if(e.target.name === "armor") {
+      fetch('http://localhost:8002/bots')
+      .then(res => res.json())
+      .then(data => setBots(data.sort((bot1, bot2) => bot2.armor - bot1.armor)))
+    }
+  }
 
   return (
     <div>
+      <SortBar handleSort={handleSort}/>
       <YourBotArmy 
       bots={bots.filter(bot => bot.chosen)} 
       deleteBot={handleBotDeletion} 
